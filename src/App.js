46ea439776cfc4e7338b543ae4e63dc87db64da6
Lucas Boscariole Silva <lucas.boscariole@hotmail.com>
2021-06-21 19:06:22 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select } from '@material-ui/core';
+import InfoBox from './InfoBox';
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
 
   useEffect(() => {
     const getCountries = async () => {
@@ -18,31 +20,34 @@ function App() {
         });
     };
     getCountries();
-    // Gets all countries of the API
+    // Gets all countries from the API
   }, [countries]);
+
+  const ChangeCountry = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
 
   return (
     <div className='app'>
       <div className='app__header'>
         <h1>COVID-19 TRACKER</h1>
         <FormControl className='app__dropdown'>
-          <Select
-            variant='outlined'
-            value='abc'
-            onChange={(e) => console.log(e)}
-          >
+          <Select variant='outlined' value={country} onChange={ChangeCountry}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
             {countries.map((country) => {
               return <MenuItem value={country.value}>{country.name}</MenuItem>;
             })}
           </Select>
         </FormControl>
       </div>
-      {/* Header */}
-      {/* Title + Select input dropdown */}
+      {/* Header Ends*/}
 
-      {/* Infobox */}
-      {/* Infobox */}
-      {/* Infobox */}
+      <div className='app__stats'>
+        <InfoBox title='Coronavirus cases' cases={123} total={2000} />
+        <InfoBox title='Recovered' cases={123} total={3000} />
+        <InfoBox title='Deaths' cases={123} total={4000} />
+      </div>
 
       {/* table */}
       {/* graph */}
